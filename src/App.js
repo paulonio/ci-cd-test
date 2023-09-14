02170a7e1abb5a4b675b316e368f4e769acc7c1e
Pavel <paul.dashkevich@yandex.ru>
2023-09-14 16:53:25 +0300

    !!!!

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <p>Some text</p>
+      <p>Some text!!!</p>
       <input value={state} onChange={(e) => setState(e.target.value)} />
       <h1>Hello World!!!!</h1>
       <h2>... .... and React!</h2>
