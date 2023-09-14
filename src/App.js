6f4d76be21dbea6eaf3452dafd401e19d7fc0e6e
Pavel <paul.dashkevich@yandex.ru>
2023-09-14 19:46:26 +0300

    squashed commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React, { useState } from "react";
 function App() {
   const [state, setState] = useState('');
 
+  console.log(111);
+  console.log(222);
+  console.log(333);
+
   return (
     <>
       <p>Some text!!!</p>
