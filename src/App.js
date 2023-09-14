ccc7d255ff9de419227a7b5741ce3f1e2f6c8ab7
Pavel <paul.dashkevich@yandex.ru>
2023-09-14 19:59:00 +0300

    branch squash

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import React, { useState } from "react";
 function App() {
   const [state, setState] = useState('');
 
+  console.log(555);
+  console.log(777);
+  console.log(888);
+  console.log(999);
+
   return (
     <>
       <p>Some text!!!</p>
